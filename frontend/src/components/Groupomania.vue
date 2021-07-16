e47c44283d864df2b3730e89c523cd3d5d58c7a7
Sh1r0w <k4leth@gmail.com>
2021-07-16 23:44:44 +0200

    first commit

---
+++ frontend/src/components/Groupomania.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <h1>{{ msg }}</h1>
+      <div id="connection">
+      Votre Mail: <input type="text" id="login" required/>
+      Votre mot de passe:<input type="text" id="password" required/>
+      <input type="submit" id="validLogin">
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Groupomania',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+a {
+  color: #112341;
+}
+
+</style>
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/icon-left-font-monochrome-black.svg">
+    <Groupomania msg="Bienvenu sur Groupomania"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Groupomania from '@/components/Groupomania.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Groupomania
+  }
+}
+</script>
